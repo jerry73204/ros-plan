c7207beac05cede94580fbf3d5bcd125545d210a
aeon <jerry73204@gmail.com>
2025-10-15 16:58:03 +0800

    Remove rmw_introspect_cpp and ros2-introspect pacakges

--- ros2/rmw_introspect_cpp/include/rmw_introspect/identifier.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef RMW_INTROSPECT__IDENTIFIER_HPP_
-#define RMW_INTROSPECT__IDENTIFIER_HPP_
-
-// Identifier must be a single symbol that all compilation units reference
-// Using extern ensures pointer equality works for identifier comparison
-extern "C" const char * const rmw_introspect_cpp_identifier;
-
-#endif  // RMW_INTROSPECT__IDENTIFIER_HPP_
